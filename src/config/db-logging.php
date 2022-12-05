038d70ad5c3ce318e8d6ac311642eeb30cbdceed
ironp <devascovitche@gmail.com>
2022-12-05 14:10:04 +0200

    merge logging config fix

--- src/config/db-logging.php
+++ src/config/db-logging.php
@@ -3,6 +3,8 @@
 return [
     'domain' => env('MAILGUN_DOMAIN'),
     'secret' => env('MAILGUN_SECRET'),
+    'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    'scheme' => 'https',
     'public' => env('MAILGUN_PUBLIC')
 ];
 
